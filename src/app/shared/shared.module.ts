6149e97685803c1f38dd00f7715071b2bde1c8c1
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2023-12-18 13:00:33 +0100

    Eliminando ficheros innecesarios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SideBarComponent } from './pages/side-bar/side-bar.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { RouterModule } from '@angular/router';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { LoadingSpinerComponent } from './components/loading-spiner/loading-spiner.component';
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     SideBarComponent,
-    AboutPageComponent,
-    ContactPageComponent,
     SearchBoxComponent,
     LoadingSpinerComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [
-    HomePageComponent,
     SideBarComponent,
-    AboutPageComponent,
-    ContactPageComponent,
     SearchBoxComponent,
     LoadingSpinerComponent,
 
